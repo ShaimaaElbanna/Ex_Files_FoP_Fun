6c1729252883ebde200100c0635edfa4ea77b73c
ShaimaaElbanna <ShaimaaElbanna@users.noreply.github.com>
2018-10-09 14:46:56 +0200

    ok

--- Exercise Files/ch06/While loop/script.js
+++ Exercise Files/ch06/While loop/script.js
@@ -1,1 +1,30 @@
 
+//set up the index
+var i = 1;
+while ( i < 10){ //check the condition
+    //do stuff
+    //do stuff
+    //do stuff
+    //do stuff
+    //etc..
+    i++ //incresment the index
+}
+
+
+//for loop
+  //setup-index; //check-Condition; //incresment-index
+for (var i = 1;      i < 100 ;         i++){
+ //do stuff
+    //do stuff
+    //do stuff
+    //do stuff
+    //etc..
+} 
+
+//do--- while loop
+
+var a = 1;
+do {
+    //do stuff
+    a++;
+} while (a < 10);
