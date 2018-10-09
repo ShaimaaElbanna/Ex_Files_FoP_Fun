4356f340bee670b39839add3dc4822dbe44a0ef8
ShaimaaElbanna <ShaimaaElbanna@users.noreply.github.com>
2018-10-09 11:33:39 +0200

    Initial commit

---
+++ Exercise Files/ch04/switch statement/script.js
@@ -0,0 +1,16 @@
+var grade = "Regular";
+	
+switch (grade) {
+      case "Regular":
+            alert("It's a $3.15")
+            break;
+      case "Premium":
+           alert("It's a $3.35")
+           break;
+      case "Diesel":
+           alert("It's a $3.47")
+           break;
+      default:
+            alert("That’s not a valid grade")
+}
+//defult means if it’s not any od the above cases do the next code
